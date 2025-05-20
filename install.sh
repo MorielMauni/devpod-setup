335fc692f32e1b1681190cd41f1a0382749699cc
Moriel Mauni <moriel.ma0@gmail.com>
2025-05-20 23:26:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,8 +14,31 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 # Fix shallow clone to allow full updates
 git -C /home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-core fetch --unshallow || true
 
-# Tap required repos
-brew tap fluxcd/tap
+# Aliases
+cat << 'EOF' >> ~/.bashrc
+alias gs='git status'
+alias n='nvim'
+
+# Kubectl
+alias k='kubectl'
+source /etc/bash_completion.d/000_bash_completion_compat.bash
+source <(kubectl completion bash)
+complete -o default -F __start_kubectl k
+
+alias kgp='kubectl get pods'
+alias kc='kubectx'
+alias kn='kubens'
+
+alias kcs='kubectl config use-context admin@homlab-staging'
+alias kcp='kubectl config use-context admin@homelad-production'
+alias kcontext='kubectl config use-context'
+
+alias devpod='devpod-cli'
+
+alias la='ls -la'
+alias ll='ls -l'
+
+EOF
 
 # Install packages
 brew install \
